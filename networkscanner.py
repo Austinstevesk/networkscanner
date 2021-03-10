e26c5bbb06148c040659c12455ec467a7a612e66
Austinstevesk <45067454+Austinstevesk@users.noreply.github.com>
2021-03-10 23:21:27 +0300

    Update networkscanner.py

--- networkscanner.py
+++ networkscanner.py
@@ -2,7 +2,7 @@ import scapy.all as scapy
 request = scapy.ARP()
 request.pdst = '192.168.205.255/22' #Your IP Address
 broadcast = scapy.Ether()
-broadcast.dst = 'ff:ff:ff:ff:ff:ff'
+broadcast.dst = 'ff:ff:ff:ff:ff:ff' #Sends a broadcast message
 packet = request/broadcast
 client = scapy.srp(packet, timeout=3, verbose=0)
 print("IP "+" "*18+"Mac")
